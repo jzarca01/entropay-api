707a098329105f7f32f0a88ec4c1f5e38788b900
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-03-21 13:39:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Entropay API
+
+Helpers for Entropay
+
+## Account creation
+
+```
+node entropay.js --email youraccount@email.com --username yourusername --password youraccount@password 
+```
---
+++ entropay.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const program = require('commander');
+const clear = require('clear');
+const emojic = require("emojic")
+
+const entropayApi = require('./lib/entropay-api');
+
+let ERRORS = [];
+
+program
+  .version('1.0')
+  .option('-e, --email [value]', 'Specify an email address')
+  .option('-u --username [value]', 'Specify a username')
+  .option('-p, --password [value]', 'Specify a password')
+  .parse(process.argv);
+
+if(!program.email || !program.username || !program.password ) {
+    ERRORS.push("Please provide valid email or username or password");
+}
+
+if(ERRORS.length > 0) {
+    clear();
+    console.log(ERRORS.join("\n"));
+    return;
+}
+
+entropayApi.signup(program.username, program.email, program.password, { birthDay: 1, birthMonth: "Jan", birthYear: "1980"})
+.then(email => {
+    console.log(emojic.whiteCheckMark + "\nAccount created");
+})
+.catch(error => {
+    console.log(emojic.x + `\nAn error occured: ${error}`);
+    return;
+});
